931c394cacb539743ecd08284e489cc76673354d
william <willadan667@gmail.com>
2025-07-28 15:05:13 +0000

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function App() {
   return (
       <main>
         {/* drawer nav bar */}
-        <div className="drawer">
+        <div className="drawer font-">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
               {/* Navbar */}
@@ -117,7 +117,7 @@ function App() {
                     </svg>
                   </label>
                 </div>
-                <div className="mx-2 flex-1 px-2 font-extrabold text-primary">Welcome</div>
+                <div className="mx-2 flex-1 px-2 font-extrabold text-primary font-work-sans">Welcome</div>
                 <div className="hidden flex-none lg:block">
                   <ul className="menu menu-horizontal">
               {/* Navbar menu content here */}
@@ -127,7 +127,7 @@ function App() {
                       src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
                       alt="App Logo"
                       className="w-5"
-                    /> <p className='text-primary font-bold'>my github</p>
+                    /> <p className='text-primary font-bold font-work-sans'>my github</p>
                   </a>
                    
                 </li>
@@ -141,17 +141,16 @@ function App() {
                   <div className="hero-content flex-col lg:flex-row lg:gap-20">
                     <img className="mask mask-hexagon-2 w-62 lg:w-1/5" src={profile} alt="Profile" />
                     <div>
-                      <h1 className="text-5xl font-bold text-primary mb-2">
+                      <h1 className="text-5xl font-bold text-primary mb-2 font-montserrat">
                         Hi there! 
                       </h1>
-                      <h2 className="text-3xl font-semibold text-secondary mb-4">
+                      <h2 className="text-3xl font-semibold text-secondary mb-4 font-montserrat">
                         I'm William Adangabe
                       </h2>
-                      <p className="py-2 text-lg text-neutral">
-                        A <span className="font-bold text-primary">backend developer</span> focused on building robust and scalable systems.<br />
-                        Currently expanding my skills into the <span className="font-bold text-secondary">frontend</span> world to become a more versatile developer.
-                      </p>
-                      <button className="btn btn-outline btn-primary mt-4">Donwload CV</button>
+                      <p className="py-2 text-lg text-neutral font-work-sans">
+                        A <span className="font-bold text-primary">backend developer</span> focused on building robust and scalable systems, <br />
+                       complemented by a deepening command of<span className="font-bold text-secondary"> frontend</span>  development. </p>
+                      <button className="btn btn-outline btn-primary mt-4 font-work-sans">Donwload CV</button>
                     </div>
                   </div>
                 </div>
@@ -162,14 +161,14 @@ function App() {
                 {/* about me */}
               <div className="p-10 bg-base-200">
               <div className="max-w-3xl mx-auto text-center">
-                <h2 className="text-4xl font-bold text-primary mb-4">About Me</h2>
-                <p className="text-lg text-neutral leading-relaxed">
-                  I’m a <span className="font-semibold text-secondary">Fullstack Developer</span> passionate about creating
+                <h2 className="text-4xl font-bold text-primary mb-4 font-montserrat">About Me</h2>
+                <p className="text-lg text-neutral leading-relaxed font-work-sans">
+                  I’m a <span className="font-semibold text-secondary font-work-sans">Fullstack Developer</span> passionate about creating
                   efficient, scalable, and user-friendly web solutions. With experience in both
-                  <span className="font-bold text-primary"> frontend</span> and
-                  <span className="font-bold text-primary"> backend</span> development, I build seamless applications that deliver real value.
+                  <span className="font-bold text-primary font-work-sans"> frontend</span> and
+                  <span className="font-bold text-primary font-work-sans"> backend</span> development, I build seamless applications that deliver real value.
                 </p>
-                <p className="text-lg text-neutral mt-4 leading-relaxed">
+                <p className="text-lg text-neutral mt-4 leading-relaxed font-work-sans">
                   I’m open to collaboration and always ready to contribute to innovative projects.
                   Currently seeking opportunities to join a dynamic team or work on exciting freelance ventures.
                 </p>
@@ -178,8 +177,8 @@ function App() {
 
           {/* Skills */}
           <div>
-            <h1 className='text-center font-extrabold text-4xl text-primary mt-6 mb-5 '>My Skills</h1>
-              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 bg-base-100 p-6  max-w-7xl mx-auto">
+            <h1 className='text-center font-extrabold text-4xl text-primary mt-6 mb-5 font-montserrat '>My Skills</h1>
+              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 bg-base-100 p-6  max-w-7xl mx-auto font-work-sans">
             {skills.map((skill, idx) => (
               <div key={idx} className="flex flex-col items-center p-4 bg-base-200 rounded-lg">
                 <img src={skill.src} alt={skill.skill} className="w-10 h-10 mb-2" />
@@ -193,10 +192,10 @@ function App() {
 
         {/* projects */}
      <div className="w-full flex flex-col items-center">
-  <h1 className='text-center font-extrabold text-4xl text-primary mt-6 mb-5'>Projects</h1>
+  <h1 className='text-center font-extrabold text-4xl text-primary mt-6 mb-5 font-montserrat'>Projects</h1>
 
   <div className="w-full flex justify-center">
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 max-w-screen-xl w-full'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 max-w-screen-xl w-full font-work-sans'>
       {projects.map((project, idx) => (
         <div key={idx} className="card bg-base-100 shadow-xl">
           <figure>
@@ -234,7 +233,7 @@ function App() {
 </div>
 
       {/* contact me */}
-      <form className="max-w-xl mx-auto bg-base-100 p-6 rounded-xl shadow-lg space-y-4 mt-10  " action="https://formsubmit.co/willadan667@gmail.com" method="POST">
+      <form className="font-work-sans max-w-xl mx-auto bg-base-100 p-6 rounded-xl shadow-lg space-y-4 mt-10  " action="https://formsubmit.co/willadan667@gmail.com" method="POST">
             <h2 className="text-2xl font-bold text-center text-primary">Contact Me</h2>
 
             <div className="form-control">
@@ -276,7 +275,7 @@ function App() {
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="App Logo" />
         </a>
       </div>
-      <span className="font-bold text-lg text-primary">My Github</span>
+      <span className="font-bold text-lg text-primary font-montserrat">My Github</span>
     </div>
     {/* Navigation menu */}
     <ul className="flex-1 space-y-2">
@@ -285,7 +284,7 @@ function App() {
           <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
             <path d="M8 17l4 4 4-4m0-5V3a1 1 0 00-1-1h-6a1 1 0 00-1 1v9m10 4h-4m-4 0H4" />
           </svg>
-          <span className='font-bold text-neutral'>Projects</span>
+          <span className='font-bold text-neutral font-work-sans'>Projects</span>
         </a>
       </li>
       <li>
@@ -293,7 +292,7 @@ function App() {
           <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
             <path d="M5.121 17.804A13.937 13.937 0 0112 15c2.485 0 4.797.657 6.879 1.804M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
-          <span className='font-bold text-neutral'>About me</span>
+          <span className='font-bold text-neutral font-work-sans'>About me</span>
         </a>
       </li>
       {/* Add more menu items as needed */}
@@ -302,7 +301,7 @@ function App() {
            <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
             <path d="M5.121 17.804A13.937 13.937 0 0112 15c2.485 0 4.797.657 6.879 1.804M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
-          <span className='font-bold text-neutral'>Email</span>
+          <span className='font-bold text-neutral font-work-sans'>Email</span>
         </a>
       </li>
     </ul>
